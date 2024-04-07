589b4951c99d3d615fa24e32894593178b32d98d
Mathias Fontain <mathias.fontain@vol.at>
2024-04-07 16:40:13 +0200

    add model, middleware

--- index.js
+++ index.js
@@ -2,122 +2,21 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const config = require('./utils/config')
-const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 
-const url = config.MONGODB_URI
-
-mongoose.set('strictQuery', false)
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-const wordSchema = new mongoose.Schema({
-  text: String,
-  type: String,
-})
-
-wordSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  },
-})
-
-const Word = mongoose.model('Word', wordSchema)
-
-let words = {
-  articles: [
-    'the',
-    'a',
-    'my',
-    'his',
-    'her',
-    'our',
-    'their',
-    'no',
-    'one',
-    'this',
-    'that',
-  ],
-  adjectives: [
-    'cool',
-    'hungry',
-    'slow',
-    'nice',
-    'thin',
-    'young',
-    'wrong',
-    'sarcastic',
-    'beautiful',
-    'red',
-    'dead',
-  ],
-  objects: [
-    'bird',
-    'clock',
-    'boy',
-    'cow',
-    'pineapple',
-    'horse',
-    'astronaut',
-    'duck',
-    'teacher',
-    'lady',
-    'professor',
-    'hamster',
-    'dog',
-    'boat',
-  ],
-  verbs: [
-    'kicks',
-    'eats',
-    'loves',
-    'likes',
-    'hates',
-    'misses',
-    'visits',
-    'beats',
-    'hugs',
-    'kills',
-  ],
-  subjects: [
-    'professors',
-    'agents',
-    'directors',
-    'animals',
-    'cinemas',
-    'stars',
-    'plants',
-    'water',
-    'fire',
-    'mountains',
-  ],
-  adverbs: [
-    'slowly',
-    'elegantly',
-    'precisely',
-    'quickly',
-    'sadly',
-    'humbly',
-    'proudly',
-    'shockingly',
-    'calmly',
-    'passionately',
-  ],
-}
+const Word = require('./models/word')
 
 app.use(cors())
+app.use(express.json())
+app.use(middleware.errorHandler)
 
-app.get('/api/words', (request, response) => {
-  Word.find({}).then((words) => {
-    response.json(words)
-  })
+app.get('/api/words', async (request, response, next) => {
+  try {
+    const result = await Word.find({})
+    response.json(result)
+  } catch (error) {
+    next(error)
+  }
 })
 
 const PORT = config.PORT
---
+++ models/word.js
@@ -0,0 +1,28 @@
+const config = require('../utils/config')
+
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const wordSchema = new mongoose.Schema({
+  text: String,
+  type: String,
+})
+
+wordSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Word', wordSchema)
